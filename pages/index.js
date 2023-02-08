71f0b26f322c440d82c2dcfe472f9c04909fe147
kylelee2 <60560399+kylelee2@users.noreply.github.com>
2023-02-08 09:02:04 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,22 +5,47 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>SOON TO BE MASTR</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <head>
+        <title>My Web Page</title>
+      </head>
 
-      <main>
-        <Header title="AI DEPLOY" />
-        <p className="description">
-          Outline for Webpage and Business Model
-        </p>
-    
-        <p>1. header, with contact, ai, quiz, test, exam</p>
-        <p>2. google ads, membership based rev model</p>
-      </main>
+      <body>
+        <header>
+          <nav>
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Services</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </nav>
+        </header>
 
-      <Footer />
+        <main>
+          <!-- Add your main content here -->
+        </main>
+
+        <footer>https://github.com/kylelee2/next-netlify-starter/blob/main/pages/index.js
+          <div class="contact-info">
+            <h3>Contact Information</h3>
+            <p>Phone: (123) 456-7890</p>
+            <p>Email: info@mywebpage.com</p>
+            <p>Address: 123 Main St, Anytown, USA 12345</p>
+          </div>
+          <div class="policy-agreement">
+            <h3>Policy Agreement</h3>
+            <p><a href="#">Terms of Service</a></p>
+            <p><a href="#">Privacy Policy</a></p>
+          </div>
+          <div class="social-media">
+            <h3>Follow Us</h3>
+            <p><a href="#">Facebook</a></p>
+            <p><a href="#">Twitter</a></p>
+            <p><a href="#">Instagram</a></p>
+          </div>
+        </footer>
+
+      </body>
     </div>
   )
 }
